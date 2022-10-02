4c27690a67d81f6d1cc9fbdcf5cd17ba5b3fd192
dzejpi <dzejjpii@gmail.com>
2022-10-02 13:08:26 +0200

    Updated game loop.

--- scripts/GameLoop.gd
+++ scripts/GameLoop.gd
@@ -45,5 +45,49 @@ func _process(delta):
 		55:
 			GlobalVar.game_dialog = "A cheap, thoughtless, gift that nobody ever asks for."
 			GlobalVar.game_dialog_countdown = 5
+		70:
+			GlobalVar.game_dialog = "We have enough wine. We need jam and tea now."
+			GlobalVar.game_dialog_countdown = 5
+			GlobalVar.is_4_working = true
+			GlobalVar.is_6_working = true
+			GlobalVar.is_1_working = false
+		90:
+			GlobalVar.game_dialog = "Super sorry! We forgot about the soap."
+			GlobalVar.game_dialog_countdown = 10
+			GlobalVar.is_5_working = true
+		120:
+			GlobalVar.game_dialog = "You seem super busy. We should slow down."
+			GlobalVar.game_dialog_countdown = 10
+			GlobalVar.game_speed = 0.80
+		140:
+			GlobalVar.game_dialog = "We should slow even more."
+			GlobalVar.game_dialog_countdown = 10
+			GlobalVar.game_speed = 0.50
+		154:
+			GlobalVar.game_dialog = "We need to package wine now too."
+			GlobalVar.game_dialog_countdown = 10
+			GlobalVar.game_speed = 0.50
+			GlobalVar.is_1_working = true
+		170:
+			GlobalVar.game_dialog = "We need to speed up."
+			GlobalVar.game_dialog_countdown = 10
+			GlobalVar.game_speed = 0.80
+		190:
+			GlobalVar.game_dialog = "Normal speed now!"
+			GlobalVar.game_dialog_countdown = 10
+			GlobalVar.game_speed = 1
+		220:
+			GlobalVar.game_dialog = "Good news! No need for soap anymore."
+			GlobalVar.game_dialog_countdown = 10
+			GlobalVar.game_speed = 1
+			GlobalVar.is_5_working = false
+		260:
+			GlobalVar.game_dialog = "Nevermind, customers changed their mind."
+			GlobalVar.game_dialog_countdown = 10
+			GlobalVar.game_speed = 1
+		300:
+			GlobalVar.game_dialog = "What is happening? Spacetime seems to be warping up."
+			GlobalVar.game_dialog_countdown = 10
+			GlobalVar.game_speed = 2
 
 	elapsed_time += (game_loop_speed * delta)
